88401c0874dfef0472cc4fa2562e85d8fee0541a
John M. Knutson Sr <john.m.knutson@gmail.com>
2021-08-01 11:30:30 -0500

    update to whats actually running

--- code.py
+++ code.py
@@ -1,38 +1,38 @@
+"""CircuitPython Essentials HID Keyboard example"""
 import time
-
+ 
 import board
 import digitalio
 import usb_hid
-import rotaryio
-
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
 from adafruit_hid.keycode import Keycode
-
-encoder = rotaryio.IncrementalEncoder(board.A0, board.A4)
-last_position = None
-
-keypress_pins = [
-    board.A1,
-    board.A2,
-    board.A3
-]
-
+ 
+# The pins we'll use, each will have an internal pullup
+keypress_pins = [board.A1, board.A2]
+# Our array of key objects
 key_pin_array = []
-mic_hot = False
-video_hot = False
-time.sleep(1)
-keyboard = Keyboard(usb_hid.devices)
-keyboard_layout = KeyboardLayoutUS(keyboard)
+# The Keycode sent for each button, will be paired with a control key
 
+# inital mic/mute state
+mic_hot = False
+ 
+# The keyboard object!
+time.sleep(1)  # Sleep for a bit to avoid a race condition on some systems
+keyboard = Keyboard(usb_hid.devices)
+keyboard_layout = KeyboardLayoutUS(keyboard)  # We're in the US :)
+ 
+# Make all pin objects inputs with pullups
 for pin in keypress_pins:
     key_pin = digitalio.DigitalInOut(pin)
     key_pin.direction = digitalio.Direction.INPUT
     key_pin.pull = digitalio.Pull.UP
     key_pin_array.append(key_pin)
-
+ 
+# For most CircuitPython boards:
 led = digitalio.DigitalInOut(board.D13)
 led.direction = digitalio.Direction.OUTPUT
+ 
 print("Waiting for key pin...")
 
 def toggle_mute():
@@ -41,41 +41,23 @@ def toggle_mute():
     keyboard.release_all()
     mic_hot = not mic_hot
     led.value = mic_hot
-    print('toggle mute, value: %d' % mic_hot)
-
-def toggle_video():
-    global video_hot
-    keyboard.press(Keycode.COMMAND, Keycode.SHIFT, Keycode.V)
-    keyboard.release_all()
-    video_hot = not video_hot
-    # led.value = mic_hot
-    print('toggle video, value: %d' % video_hot)
 
 def leave_call():
     keyboard.press(Keycode.COMMAND, Keycode.W)
     keyboard.release_all()
     keyboard.send(Keycode.ENTER)
-    print('leaving call')
-
+ 
 while True:
     # Check each pin
     for key_pin in key_pin_array:
-        if not key_pin.value:
+        if not key_pin.value:  # Is it grounded?
             i = key_pin_array.index(key_pin)
             print("Pin #%d is grounded." % i)
-
+ 
             while not key_pin.value:
-                pass
+                pass  # Wait for it to be ungrounded!
             if i == 0:
                 leave_call()
-            elif i == 1:
-                toggle_video()
             else:
                 toggle_mute()
-
-
-    position = encoder.position
-    if last_position is None or position != last_position:
-        print(position)
-    last_position = position
     time.sleep(0.1)
